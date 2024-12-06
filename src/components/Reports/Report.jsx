417a06605a00f8fbe1a13c0931235a26e1e48523
olxovikov <2olxovikov2@mail.ru>
2024-12-06 16:54:06 +0300

    edit button update

--- src/components/Reports/Report.jsx
+++ src/components/Reports/Report.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react';
 import ReactTextareaAutosize from 'react-textarea-autosize'
 import styles from './Report.module.css'
 import edit from '../../img/pen-to-square-solid.svg'
@@ -7,20 +6,27 @@ import edit from '../../img/pen-to-square-solid.svg'
 function Report(props) {
     const [textArea, setTextArea] = useState(props.text);
     const [stopEdit, setStopEdit] = useState(true)
+    const textareaRef = useRef(null)
 
     useEffect(()=>{
-    
         let rez = (props.reports).map((element)=>{
             if (props.id === element.id) {return {...element, text: textArea}}
             else {return {...element}}
         })
         props.setReports(rez)
-
         }, [textArea])
 
     useEffect(()=>{
         setTextArea(props.text)
     }, [props.text])
+
+    useEffect(()=>{
+        if (!stopEdit && textareaRef.current) {
+            textareaRef.current.focus();
+            const len = textareaRef.current.value.length;
+            textareaRef.current.setSelectionRange(len, len);
+        }
+    }, [stopEdit])
 
     function changeEditMode(event) {
         event.stopPropagation()
@@ -29,7 +35,6 @@ function Report(props) {
             setStopEdit(false)
         }
         else {setStopEdit(true)}
-        
     }
 
     return (
@@ -37,7 +42,7 @@ function Report(props) {
             <label id={styles.date}>
                 {props.date}
             </label>
-            <ReactTextareaAutosize readOnly={stopEdit} onClick={stopEdit ? null : (event)=>event.stopPropagation()} value={textArea} onChange={(event)=>setTextArea(event.target.value)} style={props.isSelected ? {color: 'grey'} : {color:'rgb(17,40,65)'}}/>
+            <ReactTextareaAutosize ref={textareaRef} disabled={stopEdit} onClick={(event)=>event.stopPropagation()} value={textArea} onChange={(event)=>setTextArea(event.target.value)} style={props.isSelected ? {color: 'grey'} : {color:'rgb(17,40,65)'}}/>
             <button onClick={changeEditMode}><img style={stopEdit ? null : {filter: 'invert(79%) sepia(47%) saturate(5961%) hue-rotate(113deg) brightness(92%) contrast(105%)'}} src={edit} /></button>
         </div>
     )
